5b757bfda10010e881e42d9073875af07db9c1c1
mariobalrod <marioballestero.rdg@gmail.com>
2020-09-02 10:05:24 +0200

    feat: adding routes components to the router

--- src/containers/Router/index.tsx
+++ src/containers/Router/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Home from '../Main/Home';
+import Login from '../Guest/Login';
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/home"> </Route>
-        <Route path="/login"> </Route>
+        <Route path="/home">
+          {' '}
+          <Home />{' '}
+        </Route>
+        <Route path="/login">
+          {' '}
+          <Login />{' '}
+        </Route>
       </Switch>
     </BrowserRouter>
   );
