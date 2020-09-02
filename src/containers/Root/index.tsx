f64fdd0bb06568a09bd38dd1b9fcb2ee29108193
mariobalrod <marioballestero.rdg@gmail.com>
2020-09-02 10:40:08 +0200

    feat: adding styles to root

--- src/containers/Root/index.tsx
+++ src/containers/Root/index.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import Router from '../Router';
+import GlobalStyle from '../../styles/GlobalStyle';
+import theme from '../../styles/themes';
 
-const Root = () => <Router />;
+const Root = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router />
+    </ThemeProvider>
+  );
+};
 
 export default Root;
