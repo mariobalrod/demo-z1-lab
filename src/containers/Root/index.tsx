fc9606f9f16b9fd379b60db3fda39f62975c140c
mariobalrod <marioballestero.rdg@gmail.com>
2020-09-02 09:45:16 +0200

    feat: root and router components

---
+++ src/containers/Root/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Router from '../Router';
+
+const Root = () => (
+  <div>
+    <Router />
+  </div>
+);
+
+export default Root;
---
+++ src/containers/Router/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Router = () => {
+  return <div>Hola</div>;
+};
+
+export default Router;
